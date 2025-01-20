970eff722c9dcb778de1eb12a0d5a6ac3b05fa9c
lauritswl <lauritswl@gmail.com>
2025-01-20 09:45:57 +0100

    clean

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@
 sudo apt update
 sudo apt install jags
 Rscript AI_Reliance_DecisionMaking/setupjags.R
-git config --global user.email "lauritswl@gmail.com"
-git config --global user.name "lauritswl"
+
